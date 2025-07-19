1d08fc666e71728e47db14a0c660501559582ec4
Vishal B Shiyal <vishalbshiyal@gmail.com>
2025-07-20 02:19:10 +0530

    refactor: update route handling for production environment

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,8 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  // app.get("*", (req, res) => {
+  app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
